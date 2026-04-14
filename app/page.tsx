3edade854dc1ca97e397cf9d19856e0a3f66b23b
WebersonRodrigues7 <webersongiovani@gmail.com>
2026-04-13 21:16:03 -0300

    fix media quey

--- app/page.tsx
+++ app/page.tsx
@@ -185,6 +185,9 @@ export default function Home() {
 
         </section>
       </main>
+      <div>
+        
+      </div>
 
       <section id="contato" className={Style.contato}>
         <p className={Style.contatoLabel}>// fale comigo</p>
